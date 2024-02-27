e43e638fb8a1f97e5591eac6804b91077b41458d
zakiabdil <141113317+zakiabdil@users.noreply.github.com>
2024-02-27 08:26:11 -0800

    Delete js directory

--- js/main.js
+++
@@ -1,6 +0,0 @@
- const mntoggle = document.querySelector('.menu-toggle input');
- const nav = document.querySelector('nav ul');
-
-mntoggle.addEventListener('click',function(){
-    nav.classList.toggle('menushow');
-})
