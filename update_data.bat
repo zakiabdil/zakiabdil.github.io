4867e23d5705bfb7abc8f30b3db9065e3fa3a59f
zakiabdil <141113317+zakiabdil@users.noreply.github.com>
2024-04-15 07:30:32 -0700

    ss

--- update_data.bat
+++ update_data.bat
@@ -1,6 +1,12 @@
 @echo off
 cd /d "%~dp0"
 python get_data.py
-"C:\Users\Asus\AppData\Local\GitHubDesktop\GitHubDesktop.exe" --restore-previous-path --unity-launch
+
+REM Launch GitHub Desktop and open the repository
+START /D "C:\Users\Asus\AppData\Local\GitHubDesktop\GitHubDesktop.exe" --restore-previous-path --unity-launch --wait-for-window "GitHub Desktop"
+
+REM Commit the changes with a message
 "C:\Users\Asus\AppData\Local\GitHubDesktop\GitHubDesktop.exe" --commit-all --untracked-files --summary "Updated data.json"
+
+REM Push the committed changes to the remote GitHub repository
 "C:\Users\Asus\AppData\Local\GitHubDesktop\GitHubDesktop.exe" --push
