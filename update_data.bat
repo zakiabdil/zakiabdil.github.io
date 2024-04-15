107b552e14903df146f08c092f183f02ecce8f98
zakiabdil <141113317+zakiabdil@users.noreply.github.com>
2024-04-15 07:22:37 -0700

    ss

--- update_data.bat
+++ update_data.bat
@@ -1,3 +1,6 @@
 @echo off
 cd /d "%~dp0"
 python get_data.py
+"C:\Users\Asus\AppData\Local\GitHubDesktop\GitHubDesktop.exe" --restore-previous-path --unity-launch
+"C:\Users\Asus\AppData\Local\GitHubDesktop\GitHubDesktop.exe" --commit-all --untracked-files --summary "Updated data.json"
+"C:\Users\Asus\AppData\Local\GitHubDesktop\GitHubDesktop.exe" --push
