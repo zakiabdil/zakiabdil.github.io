ba786a5696c15d14d068f172f5440bb3d681047f
zakiabdil <141113317+zakiabdil@users.noreply.github.com>
2024-04-15 07:06:00 -0700

    tes scrap 5

---
+++ get_data.py
@@ -0,0 +1,45 @@
+from bs4 import BeautifulSoup
+import requests
+import datetime
+from datetime import datetime
+import json
+
+html_text = requests.get('https://www.republika.co.id/').text
+soup = BeautifulSoup(html_text, 'html.parser')
+
+months = ["Januari", "Februari", "Maret", "April", "Mei", "Juni", "Juli", "Agustus", "September", "Oktober",
+          "November", "Desember"]
+
+def find_news():
+    # dapatkan semua dengan class "....rder conten1"
+    news = soup.find_all('li', class_='list-group-item list-border conten1')
+    # deklarasi array
+    data = []
+    # pembukaan file json untuk menulis
+    with open('data.json', 'w') as f:
+        for new in news:
+            # strip fungsinya removing useless white space di awal dan di akhir kalimat
+            title = new.h3.text.strip()
+            categories = new.find('span', class_='kanal-info').text
+
+            current_year = str(datetime.now().date())[0:4] + " "
+            current_month = int(str(datetime.now().date())[5:7])
+            current_date = str(int(str(datetime.now().date())[9:10])) + " "
+            current_hour = str(datetime.now().isoformat())[11:13] + ":"
+            current_minute = str(datetime.now().isoformat())[14:16] + ":"
+            current_second = str(datetime.now().isoformat())[17:19]
+
+            waktuScraping = current_date + months[current_month-1] + " " + current_year + current_hour + current_minute + current_second
+            waktuPublish = new.find('div', class_='date').text.split('-')
+
+            info = {"judul": title, "kategori": categories, "waktu_publish": waktuPublish[1].strip(),
+                    "waktu_scraping": waktuScraping}
+            data.append(info)
+        # Mengubah format dictionary/json menjadi format string
+        jdumps = json.dumps(data)
+        # menuliskan data pada data.json
+        f.writelines(jdumps)
+
+
+if __name__ == '__main__':
+    find_news()
